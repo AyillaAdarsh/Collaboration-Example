be495ce6e503673bc3fdf23cf8c950e36a00025d
AyillaAdarsh <adarshayilla@gmail.com>
2023-10-14 23:15:47 +0530

    Added Variables

--- Assets/Scripts/Collaboration.cs
+++ Assets/Scripts/Collaboration.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Collaboration : MonoBehaviour
 {
+    public string Name;
+    private int Age;
+    [SerializeField] private float Salary;
     // Start is called before the first frame update
     void Start()
     {
